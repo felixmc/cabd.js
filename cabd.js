3f627ffc6e0255b8a8f35caf99352b8bb8fcc9a2
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-09-04 16:24:04 -0600

    commonjs structure + npm module

--- cabd.js
+++ cabd.js
@@ -1,17 +1,26 @@
-function cabd(source) {
-    var result = [],
-        offset = source.length % 2 === 0 ? 2 : 1;
-    for (var i = 0; i < source.length; i++) {
-        result[i] = source[i < source.length / 2 ? source.length - 2 * i - offset : 2 * i + (offset - 1) - source.length];
-    }
-    return result.join('');
-}
 
-function uncabd(source) {
-    var result = [],
-        offset = source.length % 2 === 0 ? 1 : 0;
-    for (var i = 0; i < source.length; i++) {
-        result[i] = source[i % 2 === 0 ? (source.length - offset - 1 - i) / 2 : (source.length - offset + i * 1) / 2];
+module.exports = {
+
+	obfuscate: function(source) {
+	  var result = [];
+	  var offset = source.length % 2 === 0 ? 2 : 1;
+
+	  for (var i = 0; i < source.length; i++) {
+	    result[i] = source[i < source.length / 2 ? source.length - 2 * i - offset : 2 * i + (offset - 1) - source.length];
+	  }
+
+	  return result.join('');
+	},
+
+	unobfuscate: function(source) {
+		var result = [];
+		var offset = source.length % 2 === 0 ? 1 : 0;
+
+		for (var i = 0; i < source.length; i++) {
+      result[i] = source[i % 2 === 0 ? (source.length - offset - 1 - i) / 2 : (source.length - offset + i * 1) / 2];
     }
+
     return result.join('');
-}
+	}
+
+};
